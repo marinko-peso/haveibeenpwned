e5e1d312d1c61a5a37e066688e33c701e9b0efe0
marinko <marinko.peso@gmail.com>
2018-08-12 08:10:12 +0200

    update bin

--- bin/haveibeenpwned.py
+++ bin/haveibeenpwned.py
@@ -6,22 +6,26 @@ from haveibeenpwned.status import check
 from haveibeenpwned import __version__, __summary__
 
 
-def _display_data():
+def _display_status():
     print('displaying data here')
+
+
+def _display_status_with_data():
+    print('test')
 
 
 def main():
     parser = argparse.ArgumentParser(description=__summary__)
     parser.add_argument('-v', '--version', action='store_true', help='currently installed version')
-    parser.add_argument('-r', '--raw', action='store_true', help='raw parsed data without cli')
+    parser.add_argument('-r', '--full', action='store_true', help='response will full data not just boolean, if any exists')
     args = parser.parse_args()
-    # if args.raw:
-    #     _display_scrapped_and_exit()
+    if args.full:
+        _display_status_with_data()
     if args.version:
         print(__version__)
         sys.exit(1)
 
-    _display_data()
+    _display_status()
 
 
 if __name__ == '__main__':
